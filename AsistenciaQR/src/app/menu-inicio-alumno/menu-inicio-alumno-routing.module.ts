3941abc6f31baea0dfec170d21ad58c4e60c11a0
MatiasLealTapia <matiasa.lealt.23@gmail.com>
2022-09-28 19:20:41 -0300

    intento de component

--- AsistenciaQR/src/app/menu-inicio-alumno/menu-inicio-alumno-routing.module.ts
+++ AsistenciaQR/src/app/menu-inicio-alumno/menu-inicio-alumno-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MenuInicioAlumnoPage } from './menu-inicio-alumno.page';
+import { SlideMenuComponent } from '../slide-menu/slide-menu.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MenuInicioAlumnoPage
+    component: MenuInicioAlumnoPage,
+    children:[
+      {
+        path: 'slide-menu',
+        component: SlideMenuComponent
+      }
+    ]
   }
 ];
 
--- AsistenciaQR/src/app/menu-inicio-alumno/menu-inicio-alumno.module.ts
+++ AsistenciaQR/src/app/menu-inicio-alumno/menu-inicio-alumno.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { MenuInicioAlumnoPageRoutingModule } from './menu-inicio-alumno-routing.module';
 
 import { MenuInicioAlumnoPage } from './menu-inicio-alumno.page';
+import { SlideMenuComponent } from '../slide-menu/slide-menu.component';
 
 @NgModule({
   imports: [
@@ -16,6 +17,6 @@ import { MenuInicioAlumnoPage } from './menu-inicio-alumno.page';
     IonicModule,
     MenuInicioAlumnoPageRoutingModule
   ],
-  declarations: [MenuInicioAlumnoPage]
+  declarations: [MenuInicioAlumnoPage, SlideMenuComponent]
 })
 export class MenuInicioAlumnoPageModule {}
